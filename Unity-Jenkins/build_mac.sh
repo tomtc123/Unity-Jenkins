2f8bab038a285aa9da34769271e604609e871c51
tangcheng <997063802@qq.com>
2018-09-06 11:04:51 +0800

    parse provision tool

--- Unity-Jenkins/build_mac.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "start"
-export untiy=/Applications/Unity5.5.6f1/Unity.app/Contents/MacOS/Unity
-export projectPath=/Users/tomtang/Unity-Jenkins
- 
-echo "version = $version"
- 
-$untiy -quit -batchmode -projectPath $projectPath -logFile /Users/tomtang/Unity-Jenkins/build.log  -executeMethod JenkinsBuild.Build "version=$version"
