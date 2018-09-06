a08fbeda408911e78b9bd3707b2545773962b9a9
tangcheng <997063802@qq.com>
2018-09-06 11:06:39 +0800

    parse provision tool

---
+++ Unity-Jenkins/Assets/MobileProvisionUtilities.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography;
+using System.IO;
+using System.Diagnostics;
+using System.Xml;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Represents the salient parts of a mobile provision, wrt. using it for code signing
+/// </summary>
+public class MobileProvision
+{
+    public object Tag;
+
+    public string ApplicationIdentifierPrefix = null;
+    public List<X509Certificate2> DeveloperCertificates = new List<X509Certificate2>();
+    public List<string> ProvisionedDeviceIDs;
+    public string ProvisionName;
+    public string ProvisionUUID;
+    public string ProvisionDeveloper;
+    public string ApplicationIdentifier;
+    public string ApsEnvironment;
+    public PListHelper Data;
+
+    [MenuItem("File/Build/iOS/ParseProvision")]
+    public static void ParseProvisionMenuItem()
+    {
+        var path = Application.dataPath;
+        path = path.Replace("Assets", "Build/iOS");
+        path = EditorUtility.OpenFolderPanel("choose output folder", path, "");
+        if (!string.IsNullOrEmpty(path))
+        {
+            UnityEngine.Debug.Log(">>>>>>>>>>>>>>>>sdkfolder="+path);
+            ParseProvision(path);
+        }
+    }
+
+    static Dictionary<string, string> s_mobileProvisionName = new Dictionary<string, string>()
+    {
+        {"dev.mobileprovision", "auto_build.sh" },
+        {"adhoc.mobileprovision", "AdHocExportOptions.plist" },
+        {"appstore.mobileprovision", "AppStoreExportOptions.plist" },
+    };
+
+    public static void ParseProvision(string sdkpath)
+    {
+        string templatePath = Application.dataPath;
+        templatePath = templatePath.Replace("Assets", "Build/iOS/template");
+        foreach (var item in s_mobileProvisionName)
+        {
+            var path = string.Format("{0}/AutoXcodeBuild/Certificates/{1}", sdkpath, item.Key);
+            if (File.Exists(path))
+            {
+                var bytes = File.ReadAllBytes(path);
+                var m = MobileProvisionParser.ParseFile(bytes);
+                string text = File.ReadAllText(string.Format("{0}/{1}", templatePath, item.Value));
+                text = text.Replace("REPLACE_TEAM_ID", m.ApplicationIdentifierPrefix);
+                text = text.Replace("REPLACE_UUID", m.ProvisionUUID);
+                text = text.Replace("REPLACE_PROFILE_NAME", m.ProvisionName);
+                text = text.Replace("REPLACE_DEVELOPER", m.ProvisionDeveloper);
+                text = text.Replace("REPLACE_BUNDLE_ID", m.ApplicationIdentifier);
+                File.WriteAllText(string.Format("{0}/AutoXcodeBuild/{1}", sdkpath, item.Value), text);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Extracts the dict values for the Entitlements key and creates a new full .plist file
+    /// from them (with outer plist and dict keys as well as doctype, etc...)
+    /// </summary>
+    public string GetEntitlementsString(string CFBundleIdentifier)
+    {
+        PListHelper XCentPList = null;
+        Data.ProcessValueForKey("Entitlements", "dict", delegate (XmlNode ValueNode)
+        {
+            XCentPList = PListHelper.CloneDictionaryRootedAt(ValueNode);
+        });
+
+        // Modify the application-identifier to be fully qualified if needed
+        string CurrentApplicationIdentifier;
+        XCentPList.GetString("application-identifier", out CurrentApplicationIdentifier);
+
+        if (CurrentApplicationIdentifier.Contains("*"))
+        {
+            // Replace the application identifier
+            string NewApplicationIdentifier = String.Format("{0}.{1}", ApplicationIdentifierPrefix, CFBundleIdentifier);
+            XCentPList.SetString("application-identifier", NewApplicationIdentifier);
+
+
+            // Replace the keychain access groups
+            // Note: This isn't robust, it ignores the existing value in the wildcard and uses the same value for
+            // each entry.  If there is a legitimate need for more than one entry in the access group list, then
+            // don't use a wildcard!
+            List<string> KeyGroups = XCentPList.GetArray("keychain-access-groups", "string");
+
+            for (int i = 0; i < KeyGroups.Count; ++i)
+            {
+                string Entry = KeyGroups[i];
+                if (Entry.Contains("*"))
+                {
+                    Entry = NewApplicationIdentifier;
+                }
+                KeyGroups[i] = Entry;
+            }
+
+            XCentPList.SetValueForKey("keychain-access-groups", KeyGroups);
+        }
+
+        return XCentPList.SaveToString();
+    }
+
+    /// <summary>
+    /// Constructs a MobileProvision from an xml blob extracted from the real ASN.1 file
+    /// </summary>
+    public MobileProvision(string EmbeddedPListText)
+    {
+        Data = new PListHelper(EmbeddedPListText);
+
+        // Now extract things
+
+        // Key: ApplicationIdentifierPrefix, Array<String>
+        List<string> PrefixList = Data.GetArray("ApplicationIdentifierPrefix", "string");
+        if (PrefixList.Count > 1)
+        {
+            UnityEngine.Debug.LogWarning("Found more than one entry for ApplicationIdentifierPrefix in the .mobileprovision, using the first one found");
+        }
+
+        if (PrefixList.Count > 0)
+        {
+            ApplicationIdentifierPrefix = PrefixList[0];
+        }
+
+        // Key: DeveloperCertificates, Array<Data> (uuencoded)
+        string CertificatePassword = "";
+        List<string> CertificateList = Data.GetArray("DeveloperCertificates", "data");
+        foreach (string EncodedCert in CertificateList)
+        {
+            byte[] RawCert = Convert.FromBase64String(EncodedCert);
+            DeveloperCertificates.Add(new X509Certificate2(RawCert, CertificatePassword));
+        }
+
+        // Key: Name, String
+        if (!Data.GetString("Name", out ProvisionName))
+        {
+            ProvisionName = "(unknown)";
+        }
+
+        if (!Data.GetString("UUID", out ProvisionUUID))
+        {
+            ProvisionUUID = "(unknown)";
+        }
+
+        PListHelper XCentPList = null;
+        Data.ProcessValueForKey("Entitlements", "dict", delegate (XmlNode ValueNode)
+        {
+            XCentPList = PListHelper.CloneDictionaryRootedAt(ValueNode);
+        });
+        XCentPList.GetString("application-identifier", out ApplicationIdentifier);
+        ApplicationIdentifier = ApplicationIdentifier.Replace(ApplicationIdentifierPrefix + ".", "");
+
+        XCentPList.GetString("aps-environment", out ApsEnvironment);
+        if (string.IsNullOrEmpty(ApsEnvironment))
+        {
+            UnityEngine.Debug.LogError(">>>>>>>>>>aps-environment is null");
+        }
+
+        // Key: ProvisionedDevices, Array<String>
+        ProvisionedDeviceIDs = Data.GetArray("ProvisionedDevices", "string");
+        
+        string certificates = DeveloperCertificates[0].SubjectName.Decode(X500DistinguishedNameFlags.UseUTF8Encoding);
+        ProvisionDeveloper = JenkinsBuild.GetParam(certificates.Split(','), "CN=", "");
+
+        string log = string.Empty;
+        log += string.Format("ProvisionName={0}\n", ProvisionName);
+        log += string.Format("from={0}, to={1}\n", DeveloperCertificates[0].NotBefore, DeveloperCertificates[0].NotAfter);
+        log += string.Format("ProvisionUUID={0}\n", ProvisionUUID);
+        log += string.Format("ProvisionDeveloper={0}\n", ProvisionDeveloper);
+        log += string.Format("aps-environment={0}\n", ApsEnvironment);
+        log += string.Format("certificates={0}\n", certificates);
+        UnityEngine.Debug.Log(log);
+    }
+
+    /// <summary>
+    /// Does this provision contain the specified UDID?
+    /// </summary>
+    public bool ContainsUDID(string UDID)
+    {
+        bool bFound = false;
+        foreach (string TestUDID in ProvisionedDeviceIDs)
+        {
+            if (TestUDID.Equals(UDID, StringComparison.InvariantCultureIgnoreCase))
+            {
+                bFound = true;
+                break;
+            }
+        }
+
+        return bFound;
+    }
+}
+
+/// <summary>
+/// This class understands how to get the embedded plist in a .mobileprovision file.  It doesn't
+/// understand the full format and is not capable of writing a new one out or anything similar.
+/// </summary>
+public class MobileProvisionParser
+{
+    public static MobileProvision ParseFile(byte[] RawData)
+    {
+        //@TODO: This file is just an ASN.1 stream, should find or make a raw ASN1 parser and use
+        // that instead of this (theoretically fragile) code (particularly the length extraction)
+
+        // Scan it for the start of the embedded blob of xml
+        byte[] SearchPattern = Encoding.UTF8.GetBytes("<?xml");
+        for (int TextStart = 2; TextStart < RawData.Length - SearchPattern.Length; ++TextStart)
+        {
+            // See if this point is a match
+            bool bMatch = true;
+            for (int PatternIndex = 0; bMatch && (PatternIndex < SearchPattern.Length); ++PatternIndex)
+            {
+                bMatch = bMatch && (RawData[TextStart + PatternIndex] == SearchPattern[PatternIndex]);
+            }
+
+            if (bMatch)
+            {
+                // Back up two bytes and read a two byte little endian plist size
+                int TextLength = (RawData[TextStart - 2] << 8) | (RawData[TextStart - 1]);
+
+                // Convert the data to a string
+                string PlistText = Encoding.UTF8.GetString(RawData, TextStart, TextLength);
+
+                //@TODO: Distribution provisions seem to be a byte too long, and it may be a general problem with interpreting the length
+                // For now, we just cut back to the end of the last tag.
+                int CutPoint = PlistText.LastIndexOf('>');
+                PlistText = PlistText.Substring(0, CutPoint + 1);
+
+                // Return the constructed 'mobile provision'
+                return new MobileProvision(PlistText);
+            }
+        }
+
+        // Unable to find the start of the plist data
+        UnityEngine.Debug.LogError("Failed to find embedded plist in .mobileprovision file");
+        return null;
+    }
+
+    public static MobileProvision ParseFile(Stream InputStream)
+    {
+        // Read in the entire file
+        int NumBytes = (int)InputStream.Length;
+        byte[] RawData = new byte[NumBytes];
+        InputStream.Read(RawData, 0, NumBytes);
+
+        return ParseFile(RawData);
+    }
+
+
+    public static MobileProvision ParseFile(string Filename)
+    {
+        FileStream InputStream = File.OpenRead(Filename);
+        MobileProvision Result = ParseFile(InputStream);
+        InputStream.Close();
+
+        return Result;
+    }
+
+    ///// <summary>
+    ///// Opens embedded.mobileprovision from within an IPA
+    ///// </summary>
+    //public static MobileProvision ParseIPA(string Filename)
+    //{
+    //	FileOperations.ReadOnlyZipFileSystem FileSystem = new FileOperations.ReadOnlyZipFileSystem(Filename);
+    //	MobileProvision Result = ParseFile(FileSystem.ReadAllBytes("embedded.mobileprovision"));
+    //	FileSystem.Close();
+
+    //	return Result;
+    //}
+}
---
+++ Unity-Jenkins/Assets/PListHelper.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Collections;
+
+public class PListHelper
+{
+    public XmlDocument Doc;
+
+    bool bReadOnly = false;
+
+    public void SetReadOnly(bool bNowReadOnly)
+    {
+        bReadOnly = bNowReadOnly;
+    }
+
+    public PListHelper(string Source)
+    {
+        Doc = new XmlDocument();
+        Doc.XmlResolver = null;
+        Doc.LoadXml(Source);
+    }
+
+    public static PListHelper CreateFromFile(string Filename)
+    {
+        byte[] RawPList = File.ReadAllBytes(Filename);
+        return new PListHelper(Encoding.UTF8.GetString(RawPList));
+    }
+
+    public void SaveToFile(string Filename)
+    {
+        File.WriteAllText(Filename, SaveToString(), Encoding.UTF8);
+    }
+
+    public PListHelper()
+    {
+        string EmptyFileText =
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+            "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n" +
+            "<plist version=\"1.0\">\n" +
+            "<dict>\n" +
+            "</dict>\n" +
+            "</plist>\n";
+
+        Doc = new XmlDocument();
+        Doc.XmlResolver = null;
+        Doc.LoadXml(EmptyFileText);
+    }
+
+    public XmlElement ConvertValueToPListFormat(object Value)
+    {
+        XmlElement ValueElement = null;
+        if (Value is string)
+        {
+            ValueElement = Doc.CreateElement("string");
+            ValueElement.InnerText = Value as string;
+        }
+        else if (Value is Dictionary<string, object>)
+        {
+            ValueElement = Doc.CreateElement("dict");
+            foreach (var KVP in Value as Dictionary<string, object>)
+            {
+                AddKeyValuePair(ValueElement, KVP.Key, KVP.Value);
+            }
+        }
+        else if (Value is PListHelper)
+        {
+            PListHelper PList = Value as PListHelper;
+
+            ValueElement = Doc.CreateElement("dict");
+
+            XmlNode SourceDictionaryNode = PList.Doc.DocumentElement.SelectSingleNode("/plist/dict");
+            foreach (XmlNode TheirChild in SourceDictionaryNode)
+            {
+                ValueElement.AppendChild(Doc.ImportNode(TheirChild, true));
+            }
+        }
+        else if (Value is Array)
+        {
+            if (Value is byte[])
+            {
+                ValueElement = Doc.CreateElement("data");
+                ValueElement.InnerText = Convert.ToBase64String(Value as byte[]);
+            }
+            else
+            {
+                ValueElement = Doc.CreateElement("array");
+                foreach (var A in Value as Array)
+                {
+                    ValueElement.AppendChild(ConvertValueToPListFormat(A));
+                }
+            }
+        }
+        else if (Value is IList)
+        {
+            ValueElement = Doc.CreateElement("array");
+            foreach (var A in Value as IList)
+            {
+                ValueElement.AppendChild(ConvertValueToPListFormat(A));
+            }
+        }
+        else if (Value is bool)
+        {
+            ValueElement = Doc.CreateElement(((bool)Value) ? "true" : "false");
+        }
+        else if (Value is double)
+        {
+            ValueElement = Doc.CreateElement("real");
+            ValueElement.InnerText = ((double)Value).ToString();
+        }
+        else if (Value is int)
+        {
+            ValueElement = Doc.CreateElement("integer");
+            ValueElement.InnerText = ((int)Value).ToString();
+        }
+        else
+        {
+            throw new InvalidDataException(String.Format("Object '{0}' is in an unknown type that cannot be converted to PList format", Value));
+        }
+
+        return ValueElement;
+
+    }
+
+    public void AddKeyValuePair(XmlNode DictRoot, string KeyName, object Value)
+    {
+        if (bReadOnly)
+        {
+            throw new AccessViolationException("PList has been set to read only and may not be modified");
+        }
+
+        XmlElement KeyElement = Doc.CreateElement("key");
+        KeyElement.InnerText = KeyName;
+
+        DictRoot.AppendChild(KeyElement);
+        DictRoot.AppendChild(ConvertValueToPListFormat(Value));
+    }
+
+    public void AddKeyValuePair(string KeyName, object Value)
+    {
+        XmlNode DictRoot = Doc.DocumentElement.SelectSingleNode("/plist/dict");
+
+        AddKeyValuePair(DictRoot, KeyName, Value);
+    }
+
+    /// <summary>
+    /// Clones a dictionary from an existing .plist into a new one.  Root should point to the dict key in the source plist.
+    /// </summary>
+    public static PListHelper CloneDictionaryRootedAt(XmlNode Root)
+    {
+        // Create a new empty dictionary
+        PListHelper Result = new PListHelper();
+
+        // Copy all of the entries in the source dictionary into the new one
+        XmlNode NewDictRoot = Result.Doc.DocumentElement.SelectSingleNode("/plist/dict");
+        foreach (XmlNode TheirChild in Root)
+        {
+            NewDictRoot.AppendChild(Result.Doc.ImportNode(TheirChild, true));
+        }
+
+        return Result;
+    }
+
+    public bool GetString(string Key, out string Value)
+    {
+        string PathToValue = String.Format("/plist/dict/key[.='{0}']/following-sibling::string[1]", Key);
+
+        XmlNode ValueNode = Doc.DocumentElement.SelectSingleNode(PathToValue);
+        if (ValueNode == null)
+        {
+            Value = "";
+            return false;
+        }
+
+        Value = ValueNode.InnerText;
+        return true;
+    }
+
+    public delegate void ProcessOneNodeEvent(XmlNode ValueNode);
+
+    public void ProcessValueForKey(string Key, string ExpectedValueType, ProcessOneNodeEvent ValueHandler)
+    {
+        string PathToValue = String.Format("/plist/dict/key[.='{0}']/following-sibling::{1}[1]", Key, ExpectedValueType);
+
+        XmlNode ValueNode = Doc.DocumentElement.SelectSingleNode(PathToValue);
+        if (ValueNode != null)
+        {
+            ValueHandler(ValueNode);
+        }
+    }
+
+    /// <summary>
+    /// Merge two plists together.  Whenever both have the same key, the value in the dominant source list wins.
+    /// This is special purpose code, and only handles things inside of the <dict> tag
+    /// </summary>
+    public void MergePlistIn(string DominantPlist)
+    {
+        if (bReadOnly)
+        {
+            throw new AccessViolationException("PList has been set to read only and may not be modified");
+        }
+
+        XmlDocument Dominant = new XmlDocument();
+        Dominant.XmlResolver = null;
+        Dominant.LoadXml(DominantPlist);
+
+        XmlNode DictionaryNode = Doc.DocumentElement.SelectSingleNode("/plist/dict");
+
+        // Merge any key-value pairs in the strong .plist into the weak .plist
+        XmlNodeList StrongKeys = Dominant.DocumentElement.SelectNodes("/plist/dict/key");
+        foreach (XmlNode StrongKeyNode in StrongKeys)
+        {
+            string StrongKey = StrongKeyNode.InnerText;
+
+            XmlNode WeakNode = Doc.DocumentElement.SelectSingleNode(String.Format("/plist/dict/key[.='{0}']", StrongKey));
+            if (WeakNode == null)
+            {
+                // Doesn't exist in dominant plist, inject key-value pair
+                DictionaryNode.AppendChild(Doc.ImportNode(StrongKeyNode, true));
+                DictionaryNode.AppendChild(Doc.ImportNode(StrongKeyNode.NextSibling, true));
+            }
+            else
+            {
+                // Remove the existing value node from the weak file
+                WeakNode.ParentNode.RemoveChild(WeakNode.NextSibling);
+
+                // Insert a clone of the dominant value node
+                WeakNode.ParentNode.InsertAfter(Doc.ImportNode(StrongKeyNode.NextSibling, true), WeakNode);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Returns each of the entries in the value tag of type array for a given key
+    /// If the key is missing, an empty array is returned.
+    /// Only entries of a given type within the array are returned.
+    /// </summary>
+    public List<string> GetArray(string Key, string EntryType)
+    {
+        List<string> Result = new List<string>();
+
+        ProcessValueForKey(Key, "array",
+            delegate (XmlNode ValueNode)
+            {
+                foreach (XmlNode ChildNode in ValueNode.ChildNodes)
+                {
+                    if (EntryType == ChildNode.Name)
+                    {
+                        string Value = ChildNode.InnerText;
+                        Result.Add(Value);
+                    }
+                }
+            });
+
+        return Result;
+    }
+
+    /// <summary>
+    /// Returns true if the key exists (and has a value) and false otherwise
+    /// </summary>
+    public bool HasKey(string KeyName)
+    {
+        //XmlNode DictionaryNode = Doc.DocumentElement.SelectSingleNode("/plist/dict");
+
+        string PathToKey = String.Format("/plist/dict/key[.='{0}']", KeyName);
+
+        XmlNode KeyNode = Doc.DocumentElement.SelectSingleNode(PathToKey);
+        return (KeyNode != null);
+    }
+
+    public void SetValueForKey(string KeyName, object Value)
+    {
+        if (bReadOnly)
+        {
+            throw new AccessViolationException("PList has been set to read only and may not be modified");
+        }
+
+        XmlNode DictionaryNode = Doc.DocumentElement.SelectSingleNode("/plist/dict");
+
+        string PathToKey = String.Format("/plist/dict/key[.='{0}']", KeyName);
+        XmlNode KeyNode = Doc.DocumentElement.SelectSingleNode(PathToKey);
+
+        XmlNode ValueNode = null;
+        if (KeyNode != null)
+        {
+            ValueNode = KeyNode.NextSibling;
+        }
+
+        if (ValueNode == null)
+        {
+            KeyNode = Doc.CreateNode(XmlNodeType.Element, "key", null);
+            KeyNode.InnerText = KeyName;
+
+            ValueNode = ConvertValueToPListFormat(Value);
+
+            DictionaryNode.AppendChild(KeyNode);
+            DictionaryNode.AppendChild(ValueNode);
+        }
+        else
+        {
+            // Remove the existing value and create a new one
+            ValueNode.ParentNode.RemoveChild(ValueNode);
+            ValueNode = ConvertValueToPListFormat(Value);
+
+            // Insert the value after the key
+            DictionaryNode.InsertAfter(ValueNode, KeyNode);
+        }
+    }
+
+    public void SetString(string Key, string Value)
+    {
+        SetValueForKey(Key, Value);
+    }
+
+    public string SaveToString()
+    {
+        // Convert the XML back to text in the same style as the original .plist
+        StringBuilder TextOut = new StringBuilder();
+
+        // Work around the fact it outputs the wrong encoding by default (and set some other settings to get something similar to the input file)
+        TextOut.Append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+        XmlWriterSettings Settings = new XmlWriterSettings();
+        Settings.Indent = true;
+        Settings.IndentChars = "\t";
+        Settings.NewLineChars = "\n";
+        Settings.NewLineHandling = NewLineHandling.Replace;
+        Settings.OmitXmlDeclaration = true;
+        Settings.Encoding = new UTF8Encoding(false);
+
+        // Work around the fact that it embeds an empty declaration list to the document type which codesign dislikes...
+        // Replacing InternalSubset with null if it's empty.  The property is readonly, so we have to reconstruct it entirely
+        Doc.ReplaceChild(Doc.CreateDocumentType(
+            Doc.DocumentType.Name,
+            Doc.DocumentType.PublicId,
+            Doc.DocumentType.SystemId,
+            String.IsNullOrEmpty(Doc.DocumentType.InternalSubset) ? null : Doc.DocumentType.InternalSubset),
+            Doc.DocumentType);
+
+        XmlWriter Writer = XmlWriter.Create(TextOut, Settings);
+
+        Doc.Save(Writer);
+
+        // Remove the space from any standalone XML elements because the iOS parser does not handle them
+        return Regex.Replace(TextOut.ToString(), @"<(?<tag>\S+) />", "<${tag}/>");
+    }
+}
